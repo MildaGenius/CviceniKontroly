1b05933e81a485aa6cb689633553420d01a86769
Malek, Milan <malek.milan@gmail.com>
2014-08-05 17:30:14 +0200

    Basic set of scenes.

---
+++ Assets/Scripts/UI/Splash/Splash.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Splash : MonoBehaviour
+{
+	void OnPress (bool isPressed)
+	{
+		if (isPressed)
+		{
+			Debug.Log("I was pressed on!");
+			Application.LoadLevel("TopicSelectionScene");
+		}
+	}
+}
