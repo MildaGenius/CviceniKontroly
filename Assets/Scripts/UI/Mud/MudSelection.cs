c1dc172683c26fd8124a49cf7d807295fa89a161
Milan Malek <malek.milan@gmail.com>
2014-08-05 23:38:11 +0200

    Mud selection over diagonal.

---
+++ Assets/Scripts/UI/Mud/MudSelection.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MudSelection : MonoBehaviour
+{
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OnPress (bool isPressed)
+	{
+		if (isPressed)
+		{
+			Vector2 touchPosition = UICamera.lastTouchPosition;
+			Debug.Log("I was pressed negative on position:" + touchPosition.ToString());
+			
+			if (isPositive(Vector2.zero, new Vector2(Screen.width, Screen.height), touchPosition))
+			{
+				Debug.Log("Positive");
+			}
+			else
+			{
+				Debug.Log("Negative");
+			}
+		}
+	}
+
+	public bool isPositive(Vector2 linePoint1, Vector2 linePoint2, Vector2 testPoint)
+	{
+		return ((linePoint2.x - linePoint1.x) * (testPoint.y - linePoint1.y) - (linePoint2.y - linePoint1.y) * (testPoint.x - linePoint1.x)) > 0;
+	}
+}
